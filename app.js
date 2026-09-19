d5e34520e1bbb523218efe715cddac0c465f6120
Sajani <22cis0309@ms.sab.ac.lk>
2026-09-19 18:52:06 +0530

    Add localStorage operations for customer data management

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 console.log("Hii");
+
+localStorage.setItem("name", "John Doe");
+localStorage.setItem("name", "siri");
+localStorage.setItem("age", "30");
+localStorage.setItem("city", "New York");
+
+//localStorage.removeItem("age");
+
+let customer = {
+    name: "John Doe",
+    age: 30,
+    city: "New York",
+    phone: "123-456-7890"
+};
+localStorage.setItem("customer", JSON.stringify(customer));
+
+localStorage.getItem("customer",JSON.parse(customer))
+/*
+localStorage.setItem("name", "Jjjjj");
+let cusName = localStorage.getItem("name");
+console.log(cusName);
+
+*/
+
+
